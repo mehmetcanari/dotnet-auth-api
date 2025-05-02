b0d8c07257f767db512db163c8c010b3af77f0e9
Mehmet Can Arı <bsn.mehmetcanari@gmail.com>
2025-05-02 15:28:12 +0300

    Initial Commit - Project foldering structured and workspace ready for dev

---
+++ Auth.WebAPI/Program.cs
@@ -0,0 +1,20 @@
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+
+        var app = builder.Build();
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
